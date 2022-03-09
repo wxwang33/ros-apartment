ff355211b0d326428c508b53e39e6c2a9bd95dd3
Pedro Nascimento <pnascimento@gmail.com>
2022-03-09 20:29:31 +0000

    Clean up patch

--- lib/apartment/active_record/postgresql_adapter.rb
+++ lib/apartment/active_record/postgresql_adapter.rb
@@ -5,7 +5,7 @@
 # NOTE: This patch is meant to remove any schema_prefix appart from the ones for
 # excluded models. The schema_prefix would be resolved by apartment's setting
 # of search path
-module Apartment::PostgreSqlAdapterPatch
+module Apartment::PostgreSqlAdapterSchemaPrefixPatch
   def default_sequence_name(table, _column)
     res = super
     schema_prefix = "#{Apartment::Tenant.current}."
@@ -30,10 +30,34 @@ module Apartment::PostgreSqlAdapterPatch
   end
 end
 
+module Apartment::PostgreSqlAdapterEnumPatch
+  def enum_types
+    query = <<~SQL
+      SELECT
+        type.typname AS name,
+        string_agg(enum.enumlabel, ',' ORDER BY enum.enumsortorder) AS value
+      FROM pg_enum AS enum
+      JOIN pg_type AS type
+        ON (type.oid = enum.enumtypid)
+      GROUP BY type.typname;
+    SQL
+    # Make enum values unique since each schema will have the enum declared
+    exec_query(query, "SCHEMA").cast_values.map { |name, value| [name, value.split(",").uniq.join(",")] }
+  end
+
+  # Taken from https://github.com/alassek/activerecord-pg_enum/blob/6e0daf6/lib/active_record/pg_enum/schema_statements.rb#L14-L18
+  def create_enum(name, values)
+    execute("CREATE TYPE #{name} AS ENUM (#{Array(values).map { |v| "'#{v}'" }.join(", ")})").tap {
+      reload_type_map
+    }
+  end
+end
+
 require 'active_record/connection_adapters/postgresql_adapter'
 
-# NOTE: inject this into postgresql adapters
+# NOTE: inject these into postgresql adapters
 class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
-  include Apartment::PostgreSqlAdapterPatch
+  include Apartment::PostgreSqlAdapterSchemaPrefixPatch
+  include Apartment::PostgreSqlAdapterEnumPatch if ActiveRecord.version.release >= Gem::Version.new('7.0')
 end
 # rubocop:enable Style/ClassAndModuleChildren
